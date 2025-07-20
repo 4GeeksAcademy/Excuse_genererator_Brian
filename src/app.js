bd80ee84068e6ab91c5ba9837c3f2f1e122b0a9e
Brian <brianmarinsossa131294@gmail.com>
2025-07-20 18:31:24 +0000

    generador_excusas_brian

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  let quien = ['Dayloc', 'Neff',];
+  let accion = ['se decepciono', 'felicito', 'se ilusiono','se maravillo'];
+  let que = ['de mi trabajo', 'de la comida colombniana','de la clase' ];
+  let cuando = ['en la clase', 'en el baño', 'en la oficina', 'en casa', 'cuando rezaba'];
+  
+  function randomExcuse() {
+  let quienEs = quien[Math.floor(Math.random() * quien.length)];
+  let acciones = accion[Math.floor(Math.random() * accion.length)];
+  let queEs = que[Math.floor(Math.random() * que.length)];
+  let cuandoFue = cuando[Math.floor(Math.random() * cuando.length)];
+ 
+  return `${quienEs} ${acciones} ${queEs} ${cuandoFue}`;
+  
+ 
+}
+document.getElementById("excuse").innerHTML = randomExcuse ();
+  
+}
+  
+    
+
+
